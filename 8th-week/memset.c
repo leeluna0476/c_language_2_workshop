409582a2c70429c6d08badaddda45bfb38f730b9
LEE LUNA <seojilee@student.42seoul.kr>
2025-11-10 22:07:52 +0900

    8주차 자료

---
+++ 8th-week/memset.c
@@ -0,0 +1,13 @@
+// 메모리 덩어리를 c라는 값으로 채운다.
+// 시작 주소: b
+// 덩어리 크기: bytes
+// bytes=5
+// c=2
+// [2][2][2][2][2]
+// 반환값... 시작주소 그대로.
+void *mmemset(void *b, unsigned char c, int bytes) {
+}
+
+int	main(void) {
+	return 0;
+}
