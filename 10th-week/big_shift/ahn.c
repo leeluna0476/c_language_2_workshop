e7f5a9ab58ed92800d3fcad6a8307d1f1cfa2556
LEE LUNA <seojilee@student.42seoul.kr>
2025-11-12 11:59:45 +0900

    Mr.ahn's version -- impl as a func

--- 10th-week/big_shift/ahn.c
+++ 10th-week/big_shift/ahn.c
@@ -3,9 +3,21 @@
  */
 #include <stdio.h>
 
+void left_shift_big_int(unsigned char *big_int, int Nbits) {
+	int i, Nbytes = Nbits / 8, Nbits_mod = Nbits % 8;
+	for (i = Nbytes; i < 9; ++i) {
+		big_int[i - Nbytes] = (big_int[i] << Nbits_mod) | (big_int[i + 1] >> (8 - Nbits_mod));
+	}
+	big_int[9 - Nbytes] = big_int[9] << Nbits_mod;
+	for (i = 10 - Nbytes; i < 10; ++i) {
+		big_int[i] = 0;
+	}
+}
+
 int main(void) {
 	unsigned char big_int[10];
-	for (int i = 0; i < 10; ++i) {
+	int i;
+	for (i = 0; i < 10; ++i) {
 		unsigned int u;
 		scanf("%x", &u);
 		big_int[i] = u;
@@ -13,15 +25,8 @@ int main(void) {
 
 	int Nbits;
 	scanf("%d", &Nbits);
-	int Nbytes = Nbits / 8, Nbits_mod = Nbits % 8, i;
 
-	for (i = Nbytes; i < 8; ++i) {
-		big_int[i - Nbytes] = (big_int[i] << Nbits_mod) | (big_int[i + 1] >> (8 - Nbits_mod));
-	}
-	big_int[9 - Nbytes] = big_int[i] << Nbits_mod;
-	for (i = 10 - Nbytes; i < 10; ++i) {
-		big_int[i] = 0;
-	}
+	left_shift_big_int(big_int, Nbits);
 
 	for (i = 0; i < 10; ++i) {
 		printf("0x%02x ", big_int[i]);
