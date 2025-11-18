dc7dc0e1098163d4b2409b820f132e3bc112416d
LEE LUNA <seojilee@student.42seoul.kr>
2025-11-18 17:23:21 +0900

    set uniform function interface with ahn's

--- 10th-week/big_shift/1byte_big_shift.c
+++ 10th-week/big_shift/1byte_big_shift.c
@@ -1,21 +1,36 @@
 #include <stdio.h>
 
+void single_left_shift_big_int(unsigned char *big_int) {
+	unsigned char prev_MSB = 0;
+	for (int i = 9; i >= 0; --i) {
+		unsigned char curr_MSB = big_int[i] & 0x80;
+		big_int[i] = (big_int[i] << 1) | (prev_MSB >> 7);
+		prev_MSB = curr_MSB;
+	}
+}
+
+void left_shift_big_int(unsigned char *big_int, int Nbits) {
+	while (Nbits > 0) {
+		single_left_shift_big_int(big_int);
+		--Nbits;
+	}
+}
+
 int main(void) {
-	unsigned char big_int[10] =
-		{ 0x01, 0x23, 0x45, 0x67, 0x89, 0xab, 0xcd, 0xef, 0xfe, 0xdc };
+	unsigned char big_int[10];
+	int i;
+	for (i = 0; i < 10; ++i) {
+		unsigned int u;
+		scanf("%x", &u);
+		big_int[i] = u;
+	}
 
 	int N;
 	scanf("%d", &N);
-	for (int k = 0; k < N; ++k) {
-		unsigned char prev_MSB = 0;
-		for (int i = 9; i >= 0; --i) {
-			unsigned char curr_MSB = big_int[i] & 0x80;
-			big_int[i] = (big_int[i] << 1) | (prev_MSB >> 7);
-			prev_MSB = curr_MSB;
-		}
-	}
 
-	for (int i = 0; i < 10; ++i) {
+	left_shift_big_int(big_int, N);
+
+	for (i = 0; i < 10; ++i) {
 		printf("0x%02x ", big_int[i]);
 	}
 	printf("\n");
